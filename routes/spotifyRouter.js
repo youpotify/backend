950ac1a178ca85bbbe4fd1019f8a87643f7788b0
jhyjj <jhyj3768@gmail.com>
2024-01-08 19:48:52 +0900

    feat : spotify top chart

--- routes/spotifyRouter.js
+++ routes/spotifyRouter.js
@@ -4,5 +4,6 @@ const spotifyController = require('../controllers/spotifyController');
 
 router.get('/token', spotifyController.getToken);
 router.get('/recommendations', spotifyController.getRecommendations);
+router.get('/topChart', spotifyController.getTopChart);
 
 module.exports = router;
